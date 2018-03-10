7837afeda601172a84ea9c4850f8369872adf501
uini223 <bartnik.emil@gmail.com>
2018-03-10 10:46:56 +0100

    q

--- src/controllers/panes/IndeksyPaneController.java
+++ src/controllers/panes/IndeksyPaneController.java
@@ -14,5 +14,7 @@ public class IndeksyPaneController {
 
     public void injectPanelKontrolnyViewController(PanelKontrolnyViewController p){
         this.panelKontrolnyViewController = p;
+
+
     }
 }
